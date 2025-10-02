2733ff18a259bbebd123304b163568129b9cbb08
Honnah Weber <lakeside486@protonmail.com>
2025-10-01 20:13:39 -0600

    changes to JS file

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1;
+code1 = 7 + 3
+
+let code2;
+code2 = 100 / 2 -10;
+
+let code3;
+code3 = 100%61
+
+let message = "The vault has been secured. The combination is:"
+let codeA = code1 + "-" + code2 + "-" + code3
+
+let codeB = `${code1}-${code2}-${code3}`
+
+
+console.log(message,codeA,codeB);
